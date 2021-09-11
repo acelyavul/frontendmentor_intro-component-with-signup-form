5dfe298edef4c77b0e1b694013910c6b775683f9
Acelya Vuluvan <acelyavuluvan@gmail.com>
2021-09-11 11:19:54 +0300

    first commit

---
+++ src/components/IntroSection.js
@@ -0,0 +1,16 @@
+const IntroSection = () => {
+	return (
+		<section id='intro-section'>
+			<header>
+				<h1>Learn to code by watching others</h1>
+				<p>
+					See how experienced developers solve problems in real-time. Watching
+					scripted tutorials is great, but understanding how developers think is
+					invaluable.
+				</p>
+			</header>
+		</section>
+	);
+};
+
+export default IntroSection;
