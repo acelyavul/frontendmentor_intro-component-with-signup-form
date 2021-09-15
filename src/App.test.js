c9ad18eef706da8a1d451469d8faa3002efe6201
Acelya Vuluvan <acelyavuluvan@gmail.com>
2021-09-15 20:42:01 +0300

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,42 +1,50 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 it("should render App properly", () => {
 	render(<App />);
-	const linkElement = screen.getByText(/Learn to code by watching others/i);
-	expect(linkElement).toBeInTheDocument();
+	screen.debug();
 });
 
 it("should render default elements", () => {
-	const { getByText, getByPlaceholderText } = render(<App />);
-	expect(getByText("Learn to code by watching others")).toBeDefined();
-	getByText(
-		"See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable."
-	);
-	getByText("Try it free 7 days");
-	getByPlaceholderText("First Name");
-	getByPlaceholderText("Last Name");
-	getByPlaceholderText("Email Address");
-	getByPlaceholderText("Password");
-	getByText("CLAIM YOUR FREE TRIAL");
+	render(<App />);
+	expect(
+		screen.getByText(/Learn to code by watching others/i)
+	).toBeInTheDocument();
+	expect(
+		screen.getByText(
+			/See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable./i
+		)
+	).toBeInTheDocument();
+	expect(screen.getByText(/Try it free 7 days/i)).toBeInTheDocument();
+	expect(screen.getByPlaceholderText(/First Name/i)).toBeInTheDocument();
+	expect(screen.getByPlaceholderText(/Last Name/i)).toBeInTheDocument();
+	expect(screen.getByPlaceholderText(/Email Address/i)).toBeInTheDocument();
+	expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument();
+	expect(
+		screen.getByRole("button", { name: /CLAIM YOUR FREE TRIAL/i })
+	).toBeInTheDocument();
 });
 
 it("should allow change input element", () => {
-	const { getByPlaceholderText } = render(<App />);
+	render(<App />);
 
-	const firstNameEl = getByPlaceholderText("First Name");
-	fireEvent.change(firstNameEl, "Acelya");
-	expect(firstNameEl).toBeDefined();
+	userEvent.type(screen.getByPlaceholderText(/First Name/i), "Acelya");
+	userEvent.type(screen.getByPlaceholderText(/Last Name/i), "Vul");
+	userEvent.type(
+		screen.getByPlaceholderText(/Email Address/i),
+		"acelyavuluvan@gmail.com"
+	);
+	userEvent.type(screen.getByPlaceholderText(/Password/i), "123456");
+});
 
-	const lastNameEl = getByPlaceholderText("Last Name");
-	fireEvent.change(lastNameEl, "Vul");
-	expect(lastNameEl).toBeDefined();
-
-	const email = getByPlaceholderText("Email Address");
-	fireEvent.change(email, "acelyavuluvan@gmail.com");
-	expect(email).toBeDefined();
-
-	const password = getByPlaceholderText("Password");
-	fireEvent.change(password, "123456");
-	expect(password).toBeDefined();
+it("should show errors when input elements are empty after pressing button", () => {
+	render(<App />);
+	screen.getByRole("button", { name: /CLAIM YOUR FREE TRIAL/i }).click();
+	expect(screen.getAllByText(/First Name cannot be empty/i)).toBeDefined();
+	expect(screen.getAllByText(/Last Name cannot be empty/i)).toBeDefined();
+	expect(screen.getAllByText(/Looks like this is not an email/i)).toBeDefined();
+	expect(screen.getAllByText(/Password cannot be empty/i)).toBeDefined();
+	screen.findByRole("img", { xmlns: "http://www.w3.org/2000/svg" });
 });
